d689746b4b91e04078c629d31a2436d7da6b5c79
Gabriel Sanchez <gabrielpelota.8@gmail.com>
2022-09-18 23:22:57 -0500

    add jwt

--- helpers/tokens.js
+++ helpers/tokens.js
@@ -1,6 +1,13 @@
+import jwt from 'jsonwebtoken'
+
+
+const generarJwt = (datos) => jwt.sign({id:datos.id, nombre: datos.nombre}, process.env.SECRET, {expiresIn: '1d'})
+
+
 const generarId = () => Math.random().toString(32).substring(2) + Date.now().toString(32);
 
 
 export {
-    generarId
+    generarId,
+    generarJwt
 }

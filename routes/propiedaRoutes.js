a622a601b244dc835a4bb2d863e375b5951fac75
Gabriel Sanchez <gabrielpelota.8@gmail.com>
2022-10-09 22:14:01 -0500

    add paginacion mobile y destok

--- routes/propiedaRoutes.js
+++ routes/propiedaRoutes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { body } from 'express-validator'
-import {admin,crear,guardar, agregarImagen, almacenarImagen}from '../controllers/propiedadController.js'
+import {admin,crear,guardar, agregarImagen, almacenarImagen, editar, guardarCambios, eliminar, mostrarPropiedad
+}from '../controllers/propiedadController.js'
 import protegerRuta from "../middlewares/protegerRuta.js"
 import upload from '../middlewares/subirImagen.js'
 
@@ -18,7 +19,7 @@ router.route('/propiedades/crear')
             body('titulo')
                 .notEmpty().withMessage('El titulo del anuncio es Obligatorio'),
             body('descripcion')
-                .notEmpty().withMessage('La descripcion no puede ser vacia')
+                .notEmpty().withMessage('La descripcion no puede estar vacia')
                 .isLength({max: 250}).withMessage('La Descripcion es muy Larga'),
             body('categoria')
                 .notEmpty().withMessage('Seleccione la categoria'),
@@ -38,5 +39,37 @@ router.route('/propiedades/crear')
 router.route('/propiedades/agregar-imagen/:id')
         .get(protegerRuta,agregarImagen)
         .post(protegerRuta,upload.single('imagen'),almacenarImagen)
+
+router.route('/propiedades/editar/:id')
+        .get(protegerRuta,editar)
+        .post(
+            protegerRuta,
+            body('titulo')
+                .notEmpty().withMessage('El titulo del anuncio es Obligatorio'),
+            body('descripcion')
+                .notEmpty().withMessage('La descripcion no puede estar vacia')
+                .isLength({max: 250}).withMessage('La Descripcion es muy Larga'),
+            body('categoria')
+                .notEmpty().withMessage('Seleccione la categoria'),
+            body('precio')
+                .notEmpty().withMessage('Seleccione un rango de precios'),
+            body('habitaciones')
+                .isNumeric().withMessage('Seleccione la cantidad de habitaciones'),
+            body('estacionamiento')
+                .isNumeric().withMessage('Seleccione la cantidad de estacionamientos'),
+            body('wc')
+                .isNumeric().withMessage('Seleccione la cantidad de baños'),
+            body('lat')
+                .notEmpty().withMessage('Ubica la propiedad en el mapa'),
+            guardarCambios
+            )
+router.route('/propiedades/eliminar/:id')
+    .post(protegerRuta,eliminar)
+
+//Area Publica
+
+
+router.route('/propiedad/:id')
+    .get(mostrarPropiedad)
     
 export default router

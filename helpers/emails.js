ad6c7d750cb3a7e7be5367028e9c521b2525b1e6
Gabriel Sanchez <gabrielpelota.8@gmail.com>
2022-10-23 13:30:55 -0500

    bug register

--- helpers/emails.js
+++ helpers/emails.js
@@ -23,7 +23,7 @@ const emailRegistro = async (datos) => {
             <h1>Hola ${nombre}, Bienvenido a BienesRaicesTurin </h1>
             
             <p>Tu cuenta ya esta lista, solo presiona para confirmar la cuenta en el siguiente enlace:
-            <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 8000}/auth/comprobar/${token}">Confirmar Cuenta</a>
+            <a href="${process.env.BACKEND_URL}/auth/comprobar/${token}">Confirmar Cuenta</a>
             </p>
 
             <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
